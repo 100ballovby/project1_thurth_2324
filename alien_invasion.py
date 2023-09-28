8bc26c3311e4d39c32b1bfb3cfe9265bd1a7615b
greatraksin <greatraksin@icloud.com>
2023-09-28 16:24:26 +0300

    Adding color bg

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,10 +6,12 @@ def run_game(W, H, window_name):
     pg.init()
     screen = pg.display.set_mode((W, H))
     pg.display.set_caption(window_name)
+    bg_color = (230, 230, 230)
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 sys.exit()
+        screen.fill(bg_color)
         pg.display.flip()
 
 
