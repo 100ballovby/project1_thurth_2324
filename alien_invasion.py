3f40b65f2a679bcf35506ee68e6cda891d4f989c
greatraksin <greatraksin@icloud.com>
2023-09-28 16:35:49 +0300

    Adding settings module

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,17 +1,20 @@
 import sys
 import pygame as pg
+from settings import Settings
 
 
-def run_game(W, H, window_name):
+def run_game(window_name):
     pg.init()
-    screen = pg.display.set_mode((W, H))
+    ai_settings = Settings()
+    screen = pg.display.set_mode((ai_settings.screen_width,
+                                  ai_settings.screen_height))
     pg.display.set_caption(window_name)
-    bg_color = (230, 230, 230)
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 sys.exit()
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
         pg.display.flip()
 
 
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    def __init__(self):
+        """Инициализация настроек игры"""
+        # параметры экрана игры
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+
