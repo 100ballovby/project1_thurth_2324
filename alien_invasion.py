0e94edecae7b62a07627680e30720c431e7efeb1
greatraksin <greatraksin@icloud.com>
2023-10-12 17:08:56 +0300

    Adding shooting function

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,9 +15,13 @@ def run_game(window_name):
     ship = Ship(ai_settings, screen)
     bullets = Group()  # создаем список пуль
     while True:
-        gf.check_events(ship)  # отслеживание событий мыши и клавиатуры
+        gf.check_events(ai_settings, screen, ship, bullets)  # отслеживание событий мыши и клавиатуры
         ship.update()  # постоянное опрашивает экземпляр класса Ship
         bullets.update()
+        # удаление пуль, вышедших за пределы экрана
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:  # если пуля вышла за пределы экрана
+                bullets.remove(bullet)
         gf.update_screen(ai_settings, screen, ship, bullets)
 
 
--- game_functions.py
+++ game_functions.py
@@ -1,13 +1,18 @@
 import sys
 import pygame as pg
+from bullet import Bullet
 
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Реагирует на нажатие кнопок на клавиатуре"""
     if event.key == pg.K_RIGHT:
         ship.moving_right = True
     elif event.key == pg.K_LEFT:
         ship.moving_left = True
+    elif event.key == pg.K_SPACE:
+        # создаем новую пулю и добавляем ее в группу с пулями
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ship):
@@ -17,17 +22,19 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     for event in pg.event.get():
         if event.type == pg.QUIT:
             sys.exit()
         elif event.type == pg.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pg.KEYUP:
             check_keyup_events(event, ship)
 
 
-def update_screen(settings, screen, ship):
+def update_screen(settings, screen, ship, bullets):
     screen.fill(settings.bg_color)
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
     pg.display.flip()
