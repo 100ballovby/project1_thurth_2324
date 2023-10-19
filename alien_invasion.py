67453e43fc421656373118e74b3a2dfab0183afe
greatraksin <greatraksin@icloud.com>
2023-10-19 17:19:26 +0300

    Adding alien fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,14 +14,15 @@ def run_game(window_name):
     pg.display.set_caption(window_name)
 
     ship = Ship(ai_settings, screen)
-    alien = Alien(ai_settings, screen)
+    aliens = Group()  # создаем список пришельцев
+    gf.create_fleet(ai_settings, screen, aliens, ship)
     bullets = Group()  # создаем список пуль
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)  # отслеживание событий мыши и клавиатуры
         ship.update()  # постоянное опрашивает экземпляр класса Ship
         bullets.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets, alien)
+        gf.update_screen(ai_settings, screen, ship, bullets, aliens)
 
 
 run_game('Alien invasion game')
--- game_functions.py
+++ game_functions.py
@@ -1,6 +1,7 @@
 import sys
 import pygame as pg
 from bullet import Bullet
+from alien import Alien
 
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
@@ -40,12 +41,12 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keyup_events(event, ship)
 
 
-def update_screen(settings, screen, ship, bullets, alien):
+def update_screen(settings, screen, ship, bullets, aliens):
     screen.fill(settings.bg_color)
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
-    alien.blitme()
+    aliens.draw(screen)
     pg.display.flip()
 
 
@@ -61,3 +62,41 @@ def fire_bullet(ai_settings, screen, ship, bullets):
     if len(bullets) < ai_settings.bullets_allowed:
         new_bullet = Bullet(ai_settings, screen, ship)
         bullets.add(new_bullet)
+
+
+def get_number_rows(settings, ship_height, alien_height):
+    """Определяет количество рядов, помещающихся на экране"""
+    available_space_y = (settings.screen_height - (3 * alien_height) - ship_height)
+    number_rows = int(available_space_y / (2 * alien_height))
+    return number_rows
+
+
+def get_number_aliens_x(settings, alien_width):
+    available_space_x = settings.screen_width - 2 * alien_width
+    number_of_aliens = int(available_space_x / (2 * alien_width))
+    return number_of_aliens
+
+
+def create_alien(settings, screen, aliens, alien_number, row_number):
+    alien = Alien(settings, screen)
+    alien_width = alien.rect.width
+    alien_height = alien.rect.height
+    alien.x = alien_width + (2 * alien_width) * alien_number
+    alien.rect.x = alien.x
+    alien.rect.y = alien_height + (2 * alien_height) * row_number
+    aliens.add(alien)
+
+
+def create_fleet(settings, screen, aliens, ship):
+    """Создает флот пришельцев"""
+    # Интервал между соседними пришельцами равен одной ширине пришельца
+    alien = Alien(settings, screen)
+    number_aliens_x = get_number_aliens_x(settings, alien.rect.width)
+    number_rows = get_number_rows(settings, ship.rect.height, alien.rect.height)
+
+    # создаем первый ряд пришельцев
+    for row_number in range(number_rows):
+        for alien_number in range(number_aliens_x):
+            # создание пришельца и размещение его в ряду
+            create_alien(settings, screen, aliens, alien_number, row_number)
+
